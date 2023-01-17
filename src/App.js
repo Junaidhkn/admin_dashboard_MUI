d757e2f0fe51bf0f0a5e1f0d39bbda0027f5b82b
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-17 22:30:09 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './index.css';
+import { useMode, ColorModeContext } from './theme.js';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 const App = () => {
-	return <div className='app'>HomePage</div>;
+	const [theme, colorMode] = useMode();
+
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className='app'>HomePage</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	);
 };
 
 export default App;
