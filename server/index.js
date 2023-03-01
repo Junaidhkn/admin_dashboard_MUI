a50dfaf79d5ee63d6a53987b62f9b4d979cfb6be
Junaidhkn <junaidhkniazi@gmail.com>
2023-03-02 03:19:17 +0500

    commit

--- server/index.js
+++ server/index.js
@@ -35,3 +35,14 @@ app.use( '/sales', salesRoutes )
 
 
 
+// Mongoose SetUp
+
+const PORT = process.env.PORT || 5050
+mongoose.connect( process.env.MONGO_URI, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+} ).then( () => {
+   app.listen( PORT, () => {
+      console.log( `Server Running on Port:${PORT}` );
+   } )
+} )
