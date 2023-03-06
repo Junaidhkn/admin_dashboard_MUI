960fdf97c8e26384644f8fed02810d03ddab134e
Junaidhkn <junaidhkniazi@gmail.com>
2023-03-07 00:49:44 +0500

    Navbar Added

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Box } from '@mui/material'
+import Navbar from 'components/Navbar.jsx'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
    return (
-      <div>Layout</div>
+      <Box width='100%' height='100%'>
+         <Box>
+            <Navbar />
+            <Outlet />
+         </Box>
+      </Box>
    )
 }
 
