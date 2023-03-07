de30d40200c13533a08e2275229fce82773f3d29
Junaidhkn <junaidhkniazi@gmail.com>
2023-03-07 14:01:28 +0500

    sideBar Added

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,16 +1,32 @@
-import { Box } from '@mui/material'
-import Navbar from 'components/Navbar.jsx'
-import { Outlet } from 'react-router-dom'
+import { Box, useMediaQuery } from '@mui/material';
+import Navbar from 'components/Navbar.jsx';
+import Sidebar from 'components/Sidebar.jsx';
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
-   return (
-      <Box width='100%' height='100%'>
-         <Box>
-            <Navbar />
-            <Outlet />
-         </Box>
-      </Box>
-   )
-}
+	const isNonMobile = useMediaQuery('(min-width:600px)');
+	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+	return (
+		<Box
+			display={isNonMobile ? 'flex' : 'block'}
+			width='100%'
+			height='100%'>
+			<Sidebar
+				isNonMobile={isNonMobile}
+				drawerWidth='250px'
+				isSidebarOpen={isSidebarOpen}
+				setIsSidebarOpen={setIsSidebarOpen}
+			/>
+			<Box>
+				<Navbar
+					isSidebarOpen={isSidebarOpen}
+					setIsSidebarOpen={setIsSidebarOpen}
+				/>
+				<Outlet />
+			</Box>
+		</Box>
+	);
+};
 
-export default Layout
+export default Layout;
