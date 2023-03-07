5f4a9bd8875f06c434e07f1b16ad04940efcf12d
Junaidhkn <junaidhkniazi@gmail.com>
2023-03-07 23:27:05 +0500

    commit

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,12 +1,14 @@
+import React, { useState } from 'react';
 import { Box, useMediaQuery } from '@mui/material';
-import Navbar from 'components/Navbar.jsx';
-import Sidebar from 'components/Sidebar.jsx';
-import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Navbar from 'components/Navbar';
+import Sidebar from 'components/Sidebar';
 
 const Layout = () => {
-	const isNonMobile = useMediaQuery('(min-width:600px)');
+	const isNonMobile = useMediaQuery('(min-width: 600px)');
 	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
 	return (
 		<Box
 			display={isNonMobile ? 'flex' : 'block'}
@@ -18,7 +20,7 @@ const Layout = () => {
 				isSidebarOpen={isSidebarOpen}
 				setIsSidebarOpen={setIsSidebarOpen}
 			/>
-			<Box>
+			<Box flexGrow={1}>
 				<Navbar
 					isSidebarOpen={isSidebarOpen}
 					setIsSidebarOpen={setIsSidebarOpen}
